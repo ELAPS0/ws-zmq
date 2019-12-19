0568858574b7c6dc87e05c84f19cf3dd71fe4dd1
b0b <elapso@gmail.com>
2019-12-19 17:31:29 +0000

    [+] msg_proc

--- back-srv.py
+++ back-srv.py
@@ -12,8 +12,13 @@ pull_url = 'tcp://127.0.0.1:3552'
 pub_url = 'tcp://127.0.0.1:5554'
 
 ctx = Context.instance()
-
-
+def msg_proc(msg):
+    '''
+    message processor 
+    @msg    - zmq message (list of binary string (utf-8))
+    @return   - list [topic, response_message]
+    '''
+    return [b'new event', msg[0]+b' processed']
 
 async def transmitte():
     """receive messages with polling"""
@@ -28,12 +33,9 @@ async def transmitte():
     while True:
         events = await poller.poll()
         if pull in dict(events):
-            print("recving", events)
             msg = await pull.recv_multipart()
-            print('recvd', msg)
-            print(type(msg))
-            await pub.send_multipart([b'new.event']+ msg)
-            print('transmitted')
+            await pub.send_multipart(msg_proc( msg))
+            print('recvd {}, transmetted'.format(msg))
 
 asyncio.get_event_loop().run_until_complete(asyncio.wait([
     transmitte(),
